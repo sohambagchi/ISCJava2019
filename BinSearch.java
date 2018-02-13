b441aee012d5e646d2d98a65e4dfcc6bef5ff74e
sohambagchi <sohambagchi@outlook.com>
2018-02-13 16:34:28 +0000

    BinSearch program completion attemped; not compiled yet

--- BinSearch.java
+++ BinSearch.java
@@ -13,12 +13,15 @@ public class BinSearch{
             arr[i] = input.nextInt();
         }
     }
-    void binSearch(boolean sS){
+    void binSearch(boolean sS, int term){
         int low = 0;
         int high = arr.length;
         int m = high/2;
         for(int i = low; i < high; i++){
-            
+		int s =  (low+high)/2;
+		if(arr[s] = term) sS = true;
+		else if(arr[s] > term) high = (low + s)/2;
+		else low = (high + s)/2;            
         }
     }
     public static void main(String[] args){
@@ -29,6 +32,8 @@ public class BinSearch{
         bS.input(input);
         System.out.println("Please enter the term to be searched - ");
         int se = input.nextInt();
-        bS.binSearch(sT);
+        bS.binSearch(sT, se);
+	if(sT) System.out.println("Search term " + se + " found!");
+	else System.out.println("Search term not found");
     }
 }
