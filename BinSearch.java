0b2aac1a2093daa5a6ffa0b315e7e86a31bd5340
sohambagchi <sohambagchi@outlook.com>
2018-02-15 23:17:25 +0530

    working on binary search

--- BinSearch.java
+++ BinSearch.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class BinSearch{
-    int arr[];
+    static int arr[];
     int size;
     static boolean sT;
     BinSearch(int size){
@@ -8,32 +8,54 @@ public class BinSearch{
         this.size = size;
         sT = false;
     }
+
     void input(Scanner input){
         for(int i = 0; i < size; i++){
-            arr[i] = input.nextInt();
+            arr[i] = RGen.randomInt(50, 1);
         }
     }
-    void binSearch(boolean sS, int term){
+
+    void output(int[] arrAy){
+        for(int i = 0; i < arrAy.length; i++) System.out.print(arrAy[i] + "\t");
+        System.out.println(" ");
+    }
+
+    void binSearch(int term){
         int low = 0;
         int high = arr.length;
         int m = high/2;
-        for(int i = low; i < high; i++){
-		int s =  (low+high)/2;
-		if(arr[s] = term) sS = true;
-		else if(arr[s] > term) high = (low + s)/2;
-		else low = (high + s)/2;            
+        while(low <= high){
+            if(arr[m] < term) low = m + 1;
+            else if(arr[m] > term) high = m - 1;
+            else sT = true; break;
         }
     }
+
+    void sortArray(int[] array){
+        for(int i = 1; i < array.length; i++){
+            int temp = array[i];
+            int j = i - 1;
+            while(j > -1 && array[j] > temp){
+                array[j+1] = array[j];
+                j--;
+            }
+            array[j + 1] = temp;
+        }
+    }
+
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter array size - ");
         int n = input.nextInt();
         BinSearch bS = new BinSearch(n);
         bS.input(input);
+        bS.output(arr);
+        bS.sortArray(arr);
+        bS.output(arr);
         System.out.println("Please enter the term to be searched - ");
         int se = input.nextInt();
-        bS.binSearch(sT, se);
-	if(sT) System.out.println("Search term " + se + " found!");
-	else System.out.println("Search term not found");
+        bS.binSearch(se);
+        if(sT) System.out.println("Search term " + se + " found!");
+        else System.out.println("Search term not found");
     }
 }
--- binary_search.java
+++ binary_search.java
@@ -1,21 +1,22 @@
 import java.util.*;
 public class binary_search{
-	int[] array;
-	boolean sS;
-binary_search(){
-		Scanner input = new Scanner(System.in);
-		System.out.println("Please enter the array size - "); int m = input.nextInt();
-		array = new int[m];
-			for(int i = 0; i < array.length; i++){
-			System.out.println("ARRAY [" + (i+1) + "] - ");
-			array[i] = input.nextInt();
-}
-sS = false;
-	}
-	boolean the_search(int[] arr){
-	int u = array.length;
-	int d = 0;
-	int m;
-	for(int i = 0; i < 	
-}
+    int[] array;
+    boolean sS;
+    binary_search(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the array size - "); int m = input.nextInt();
+        array = new int[m];
+        for(int i = 0; i < array.length; i++){
+            System.out.println("ARRAY [" + (i+1) + "] - ");
+            array[i] = input.nextInt();
+        }
+        sS = false;
+    }
+
+    boolean the_search(int[] arr){
+        int u = array.length;
+        int d = 0;
+        int m;
+        for(int i = 0; i < 	);
+    }
 }
