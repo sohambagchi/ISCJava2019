c37c3984523535c5b8615b0ee434735be6f423aa
sohambagchi <sohambagchi@outlook.com>
2018-05-20 21:31:33 +0530

    200518

--- Array_To_Stack.java
+++ Array_To_Stack.java
@@ -10,13 +10,47 @@ public class Array_To_Stack{
         m = new int[cap];
         st = new int[cap];
     }
+
     void input_marks(){
-        
+        Scanner input = new Scanner(System.in);
+        int temp = input.nextInt();
+        m[++top] = temp;
+        pushmarks(temp);
     }
+
     void pushmarks(int n){
         if(top < cap - 1){
-            st[++top] = n;
+            st[top] = n;
         }
-        System.out.println("Stack Overflow");
+        else System.out.println("Stack Overflow");
+    }
+
+    int popmarks(){
+        if(top == -1) return -999;
+        else return st[top--];
+    }
+
+    void display(){
+        for(int i = 0; i < cap; i++){
+            System.out.print(st[i] + "\t");
+        }
+    }
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter cap size - ");
+        int x = in.nextInt();
+        Array_To_Stack aTs = new Array_To_Stack(x);
+        int choice = -999;
+        while(choice != 0){
+            System.out.println("1) Input");
+            System.out.println("2) Pop It");
+            System.out.println("3) Show 'em all");
+            System.out.println("State your choice - ");
+            choice = in.nextInt();
+            if(choice == 1) aTs.input_marks();
+            else if(choice == 2) aTs.popmarks();
+            else aTs.display();
+        }
     }
 }
